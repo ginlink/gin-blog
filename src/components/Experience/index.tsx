29d5e7c6ef7b0d5ac567a762a54e3a1b0e81fcbc
ginlink <ginlink1998@gmail.com>
2022-05-21 18:25:54 +0800

    feat: add experience

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,40 +1,50 @@
 import React from 'react'
+import useBaseUrl from '@docusaurus/useBaseUrl'
 import { Container } from '@site/src/pages'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
 import { AutoColumn } from '../Column'
 import { TYPE } from '../Text'
+import styled from 'styled-components'
+
+const Wrapper = styled.div``
+const Img = styled.img`
+  border-radius: 50%;
+`
 
 export default function Experience() {
   return (
-    <Container>
-      <AutoColumn justify={'center'} gap={'32px'}>
-        <TYPE.largeHeader>Experience</TYPE.largeHeader>
+    <Wrapper>
+      <Container>
+        <AutoColumn justify={'center'} gap={'32px'}>
+          <TYPE.largeHeader>Experience</TYPE.largeHeader>
 
-        <VerticalTimeline lineColor={'#36cfc9'}>
-          <VerticalTimelineElement
-            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-            date={<TYPE.gray color={''}>2011 - present</TYPE.gray>}
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<div>icon</div>}
-          >
-            <h3 className="vertical-timeline-element-title">Creative Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-            <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            date="2010 - 2011"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<div>icon</div>}
-          >
-            <h3 className="vertical-timeline-element-title">Art Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-            <p>Creative Direction, User Experience, Visual Design, SEO, Online Marketing</p>
-          </VerticalTimelineElement>
-        </VerticalTimeline>
-      </AutoColumn>
-      {/* <TimeLine /> */}
-    </Container>
+          <VerticalTimeline lineColor={'#36cfc9'}>
+            <VerticalTimelineElement
+              className={'timeline-card'}
+              contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+              contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+              date={<TYPE.gray color={'var(--ifm-navbar-link-color)'}>2017.9 - 2021.6</TYPE.gray>}
+              icon={<Img src={useBaseUrl('/img/nangong.png')} />}
+            >
+              <h3>南阳理工学院</h3>
+              <h4>本科</h4>
+              <p>在校四年，自学计算机基础和前端知识</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              contentStyle={{ background: 'var(--ifm-background-color)' }}
+              date="2021.7 - 至今"
+              icon={<Img src={useBaseUrl('/img/skills/dapp.png')} />}
+            >
+              <h3>深圳云海思航科技有限公司</h3>
+              <h4>深圳，区块链前端工程师</h4>
+              <p>负责dapp程序开发</p>
+              <p>负责静态页面开发</p>
+              <p>负责面试</p>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
+        </AutoColumn>
+      </Container>
+    </Wrapper>
   )
 }
