618130b450c7dcab8f0213ed70c46c4851c3dfc8
ginlink <ginlink1998@gmail.com>
2022-06-23 09:26:32 +0800

    feat: change doc config to adapte search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,11 +4,13 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const BASE_URL = 'https://gincool.com'
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Gin's site",
   tagline: 'Gin is cool',
-  url: 'https://gincool.com',
+  url: BASE_URL,
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -129,6 +131,104 @@ const config = {
         searchPagePath: 'search',
 
         //... other Algolia params
+        rateLimit: 8,
+        startUrls: [BASE_URL],
+        sitemaps: [`${BASE_URL}/sitemap.xml`],
+        ignoreCanonicalTo: true,
+        discoveryPatterns: [`https://${BASE_URL}/**`],
+        actions: [
+          {
+            indexName: 'gincool',
+            pathsToMatch: [`https://${BASE_URL}/**`],
+            recordExtractor: ({ $, helpers }) => {
+              // priority order: deepest active sub list header -> navbar active item -> 'Documentation'
+              const lvl0 =
+                $(
+                  '.menu__link.menu__link--sublist.menu__link--active, .navbar__item.navbar__link--active'
+                )
+                  .last()
+                  .text() || 'Documentation';
+
+              return helpers.docsearch({
+                recordProps: {
+                  lvl0: {
+                    selectors: '',
+                    defaultValue: lvl0,
+                  },
+                  lvl1: ['header h1', 'article h1'],
+                  lvl2: 'article h2',
+                  lvl3: 'article h3',
+                  lvl4: 'article h4',
+                  lvl5: 'article h5, article td:first-child',
+                  lvl6: 'article h6',
+                  content: 'article p, article li, article td:last-child',
+                },
+                indexHeadings: true,
+                aggregateContent: true,
+                recordVersion: 'v3',
+              });
+            },
+          },
+        ],
+        initialIndexSettings: {
+          gincool: {
+            attributesForFaceting: [
+              'type',
+              'lang',
+              'language',
+              'version',
+              'docusaurus_tag',
+            ],
+            attributesToRetrieve: [
+              'hierarchy',
+              'content',
+              'anchor',
+              'url',
+              'url_without_anchor',
+              'type',
+            ],
+            attributesToHighlight: ['hierarchy', 'content'],
+            attributesToSnippet: ['content:10'],
+            camelCaseAttributes: ['hierarchy', 'content'],
+            searchableAttributes: [
+              'unordered(hierarchy.lvl0)',
+              'unordered(hierarchy.lvl1)',
+              'unordered(hierarchy.lvl2)',
+              'unordered(hierarchy.lvl3)',
+              'unordered(hierarchy.lvl4)',
+              'unordered(hierarchy.lvl5)',
+              'unordered(hierarchy.lvl6)',
+              'content',
+            ],
+            distinct: true,
+            attributeForDistinct: 'url',
+            customRanking: [
+              'desc(weight.pageRank)',
+              'desc(weight.level)',
+              'asc(weight.position)',
+            ],
+            ranking: [
+              'words',
+              'filters',
+              'typo',
+              'attribute',
+              'proximity',
+              'exact',
+              'custom',
+            ],
+            highlightPreTag: '<span class="algolia-docsearch-suggestion--highlight">',
+            highlightPostTag: '</span>',
+            minWordSizefor1Typo: 3,
+            minWordSizefor2Typos: 7,
+            allowTyposOnNumericTokens: false,
+            minProximity: 1,
+            ignorePlurals: true,
+            advancedSyntax: true,
+            attributeCriteriaComputedByMinProximity: true,
+            removeWordsIfNoResults: 'allOptional',
+            separatorsToIndex: '_',
+          },
+        },
       },
     }),
 };
